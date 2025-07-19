e1a362b8c6bdc91010a8982df3659b2eec396b50
Syed Sohail <sohailsyed7867123@gmail.com>
2025-07-19 19:57:02 +0530

    frontend started

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", require("./routes/authRoutes"));//done
+app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
