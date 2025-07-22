9a701fe93bd3836bfcd79a34ea9f59b019d0b288
Syed Sohail <sohailsyed7867123@gmail.com>
2025-07-22 15:06:09 +0530

    added subscription

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
+import ClickSpark from './components/ClickSpark.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClickSpark
+  sparkColor='#fff'
+  sparkSize={10}
+  sparkRadius={15}
+  sparkCount={8}
+  duration={400}
+>
+  <App />
+</ClickSpark>
     <Toaster />
   </StrictMode>,
 )
